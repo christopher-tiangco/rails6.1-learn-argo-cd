65a8eb95f0bdb74f8e9c6eb35f57950ddef0290d
Chris Tiangco <quark0x@yahoo.com>
2023-01-08 14:40:23 -0700

    Update sealed_secrets_processor.sh

--- sealed_secrets_processor.sh
+++ sealed_secrets_processor.sh
@@ -7,15 +7,27 @@
 # - Uses yq to parse secrets_mapping.yaml and creates a Sealed secrets k8s manifests
 # - Uses jq to parse GitHub secrets context object
 # - Uses kubeseal encrypter for encrypting a string
-# - Relies on the existence of environment variables set in the GitHub Actions workflow invoking this shell script
+# - Relies on the existence of environment variables set in the GitHub Actions workflow invoking this shell script.
+#   - A GitHub Actions workflow has to handle sending the secrets to the runner environment
+
+# @TODO: Error checking - throw an error if any of the following does not exist
+# - secrets_mapping.yaml
+# - $TMP_DIR
+# - $SEALED_SECRET_CONTROLLER_CERT
+# - $K8S_NAMESPACE
+
+generated_sealed_secrets_counter=0
 
 # Get all the secret names from the mapping then store to array
 secret_names=( $(yq 'keys | .[]' secrets_mapping.yaml) )
 
 for i in "${secret_names[@]}"
 do
+  sealed_secret_file_updated=false
   data_length=$(yq ".$i.data | length" secrets_mapping.yaml)
   target_filename=$(yq ".$i.sealed_secret_manifest_filename" secrets_mapping.yaml)
+  
+  #@TODO: Skip this iteration if sealed secret template (as specified by the target_filename) does not exist
 
   for (( j=0; j<$data_length; j++ ))
   do
@@ -27,11 +39,11 @@ do
     target_placeholder=$(yq ".$i.data.[$j].target_placeholder" secrets_mapping.yaml)
     sealed_secret=$(echo -n "$gh_secret_env_value" | kubeseal --cert $TMP_DIR/$SEALED_SECRET_CONTROLLER_CERT --raw --namespace $K8S_NAMESPACE --name $i | sed 's;/;\\/;g')
 
-    echo "$gh_secret_env_value"
-    printf "\n"
-    echo "Processing $i for $gh_secret_name..."
-
-    printf "\n\n"
+    #@TODO: Echo contents of sealed secret template, then using `sed` replace target_placeholder with sealed_secret then output to a file at app/manifest
+    sealed_secret_file_updated=true
   done
-  printf "\n\n"
+  
+  #@TODO: If sealed_secret_file_updated, increment generated_sealed_secrets_counter
 done
+
+echo "Generated / Updated $generated_sealed_secrets_counter sealed secret manifests"
