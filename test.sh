081327b5ae13a4a504c6d27e8c6f99c6830e382d
Chris Tiangco <quark0x@yahoo.com>
2023-01-07 21:06:50 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sealed_secret=$(echo -n ${{ secrets.RAILS_MASTER_KEY }} | kubeseal --cert ${{ env.TMP_DIR }}/${{ env.SEALED_SECRET_CONTROLLER_CERT }} --raw --namespace ${{ env.K8S_NAMESPACE }} --name rails-master-key | sed 's;/;\\/;g')
+sealed_secret=$(echo -n $RAILS_MASTER_KEY | kubeseal --cert $TMP_DIR/$SEALED_SECRET_CONTROLLER_CERT --raw --namespace $K8S_NAMESPACE --name rails-master-key | sed 's;/;\\/;g')
 echo $sealed_secret > file1
