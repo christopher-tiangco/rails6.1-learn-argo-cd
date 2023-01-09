dd6ba5eaae676c4de0d7d25c581cf755901becf6
Chris Tiangco <quark0x@yahoo.com>
2023-01-08 22:22:44 -0700

    code cleanup

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sealed_secret=$(echo -n $RAILS_MASTER_KEY | kubeseal --cert $TMP_DIR/$SEALED_SECRET_CONTROLLER_CERT --raw --namespace $K8S_NAMESPACE --name rails-master-key | sed 's;/;\\/;g')
-echo $sealed_secret > file1
