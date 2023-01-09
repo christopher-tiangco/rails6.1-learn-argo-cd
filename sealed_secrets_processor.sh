43a72b44b581cbb216c83a3deb752304d9a0b0e5
Chris Tiangco <quark0x@yahoo.com>
2023-01-08 20:39:54 -0700

    still working on sealed secrets processor

--- sealed_secrets_processor.sh
+++ sealed_secrets_processor.sh
@@ -10,33 +10,47 @@
 # - Relies on the existence of environment variables set in the GitHub Actions workflow invoking this shell script.
 #   - A GitHub Actions workflow has to handle sending the secrets to the runner environment
 
-# @TODO: Error checking - throw an error if any of the following does not exist
-# - secrets_mapping.yaml
-# - $TMP_DIR
-# - $SEALED_SECRET_CONTROLLER_CERT
-# - $K8S_NAMESPACE
+mapping_file='secrets_mapping.yaml'
+
+# Error checking - throw an error if any of the following does not exist
+if [ ! -f $mapping_file ]; then
+  echo "$mapping_file DOES NOT exist"
+  exit 1
+fi
+if [ ! -d $TMP_DIR ]; then
+  echo "$TMP_DIR DOES NOT exist"
+  exit 1
+fi
+if [ ! -f $SEALED_SECRET_CONTROLLER_CERT ]; then
+  echo "$SEALED_SECRET_CONTROLLER_CERT DOES NOT exist"
+  exit 1
+fi
+if [[ "$K8S_NAMESPACE" == "" ]]; then
+  echo "\$K8S_NAMESPACE is NOT set"
+  exit 1
+fi
 
 generated_sealed_secrets_counter=0
 
 # Get all the secret names from the mapping then store to array
-secret_names=( $(yq 'keys | .[]' secrets_mapping.yaml) )
+secret_names=( $(yq 'keys | .[]' $mapping_file) )
 
 for i in "${secret_names[@]}"
 do
   sealed_secret_file_updated=false
-  data_length=$(yq ".$i.data | length" secrets_mapping.yaml)
-  target_filename=$(yq ".$i.sealed_secret_manifest_filename" secrets_mapping.yaml)
+  data_length=$(yq ".$i.data | length" $mapping_file)
+  target_filename=$(yq ".$i.sealed_secret_manifest_filename" $mapping_file)
   
   #@TODO: Skip this iteration if sealed secret template (as specified by the target_filename) does not exist
 
   for (( j=0; j<$data_length; j++ ))
   do
-    gh_secret_name=$(yq ".$i.data.[$j].gh_secret_name" secrets_mapping.yaml)
+    gh_secret_name=$(yq ".$i.data.[$j].gh_secret_name" $mapping_file)
     gh_secret_env_value=$(eval echo '$'$gh_secret_name)
 
     [[ "$gh_secret_env_value" == "" ]] && continue
 
-    target_placeholder=$(yq ".$i.data.[$j].target_placeholder" secrets_mapping.yaml)
+    target_placeholder=$(yq ".$i.data.[$j].target_placeholder" $mapping_file)
     sealed_secret=$(echo -n "$gh_secret_env_value" | kubeseal --cert $TMP_DIR/$SEALED_SECRET_CONTROLLER_CERT --raw --namespace $K8S_NAMESPACE --name $i | sed 's;/;\\/;g')
 
     #@TODO: Echo contents of sealed secret template, then using `sed` replace target_placeholder with sealed_secret then output to a file at app/manifest
