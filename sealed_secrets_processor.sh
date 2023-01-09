f0a7c3dd03d3fde1fcd8e2bfb876714c7ececf32
Chris Tiangco <quark0x@yahoo.com>
2023-01-08 21:54:55 -0700

    still working on sealed secrets processor

--- sealed_secrets_processor.sh
+++ sealed_secrets_processor.sh
@@ -18,20 +18,24 @@ if [ ! -f $mapping_file ]; then
   echo "$mapping_file DOES NOT exist"
   exit 1
 fi
-if [ ! -d $TMP_DIR ]; then
-  echo "$TMP_DIR DOES NOT exist"
+if [ "$TMP_DIR" == "" ] || [ ! -d $TMP_DIR ]; then
+  echo "TMP_DIR environment variable DOES NOT exist or invalid path"
   exit 1
 fi
-if [ ! -f $TMP_DIR/$SEALED_SECRET_CONTROLLER_CERT ]; then
-  echo "$SEALED_SECRET_CONTROLLER_CERT DOES NOT exist"
+if [ "$SEALED_SECRET_CONTROLLER_CERT" == "" ] || [ ! -f $TMP_DIR/$SEALED_SECRET_CONTROLLER_CERT ]; then
+  echo "SEALED_SECRET_CONTROLLER_CERT environment variable DOES NOT exist or invalid filename"
+  exit 1
+fi
+if [ "$APP_MANIFEST_DIR" == "" ] || [ ! -d $APP_MANIFEST_DIR ]; then
+  echo "APP_MANIFEST_DIR environment variable DOES NOT exist or invalid path"
   exit 1
 fi
 if [ "$APP_SEALED_SECRET_TEMPLATES_DIR" == "" ] || [ ! -d $APP_SEALED_SECRET_TEMPLATES_DIR ]; then
-  echo "$APP_SEALED_SECRET_TEMPLATES_DIR DOES NOT exist"
+  echo "APP_SEALED_SECRET_TEMPLATES_DIR environment variable DOES NOT exist or invalid path"
   exit 1
 fi
 if [ "$K8S_NAMESPACE" == "" ]; then
-  echo "\$K8S_NAMESPACE is NOT set"
+  echo "K8S_NAMESPACE environment variable DOES NOT exist"
   exit 1
 fi
 
@@ -54,12 +58,14 @@ do
     gh_secret_name=$(yq ".$i.data.[$j].gh_secret_name" $mapping_file)
     gh_secret_env_value=$(eval echo '$'$gh_secret_name)
 
+    # Skip this iteration if GitHub secret does not exist
     [[ "$gh_secret_env_value" == "" ]] && continue
 
     target_placeholder=$(yq ".$i.data.[$j].target_placeholder" $mapping_file)
     sealed_secret=$(echo -n "$gh_secret_env_value" | kubeseal --cert $TMP_DIR/$SEALED_SECRET_CONTROLLER_CERT --raw --namespace $K8S_NAMESPACE --name $i | sed 's;/;\\/;g')
 
-    #@TODO: Echo contents of sealed secret template, then using `sed` replace target_placeholder with sealed_secret then output to a file at app/manifest
+    # Echo contents of sealed secret template, then using `sed` replace target_placeholder with sealed_secret then output to a file at app/manifest
+    cat $APP_SEALED_SECRET_TEMPLATES_DIR/$target_filename | sed "s/$target_placeholder/$sealed_secret/g" > $APP_MANIFEST_DIR/$target_filename
     sealed_secret_file_updated=true
   done
   
